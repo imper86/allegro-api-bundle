c8f8aa00c810869a84d5a5ad552ed29e365e0b2a
imper <me@imper.info>
2019-11-30 19:39:44 +0100

    added custom guzzle client, because of possible endless connections

--- src/Factory/AllegroServiceFactory.php
+++ src/Factory/AllegroServiceFactory.php
@@ -9,11 +9,13 @@
 namespace Imper86\AllegroApiBundle\Factory;
 
 
+use Http\Adapter\Guzzle6\Client;
 use Imper86\AllegroRestApiSdk\AllegroAuth;
 use Imper86\AllegroRestApiSdk\AllegroAuthInterface;
 use Imper86\AllegroRestApiSdk\AllegroClient;
 use Imper86\AllegroRestApiSdk\AllegroClientInterface;
 use Imper86\AllegroRestApiSdk\Model\Credentials\AppCredentials;
+use Psr\Http\Client\ClientInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\RouterInterface;
 
@@ -35,6 +37,10 @@ class AllegroServiceFactory implements AllegroServiceFactoryInterface
      * @var AppCredentials
      */
     private $credentials;
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
 
     public function __construct(array $config, RouterInterface $router, ?LoggerInterface $logger = null)
     {
@@ -47,15 +53,20 @@ class AllegroServiceFactory implements AllegroServiceFactoryInterface
             $this->router->generate($this->config['redirect_route'], [], RouterInterface::ABSOLUTE_URL),
             $this->config['sandbox']
         );
+        $this->httpClient = Client::createWithConfig([
+            'connect_timeout' => 120,
+            'read_timeout' => 120,
+            'timeout' => 180,
+        ]);
     }
 
     public function createAuth(): AllegroAuthInterface
     {
-        return new AllegroAuth($this->credentials, $this->logger);
+        return new AllegroAuth($this->credentials, $this->logger, $this->httpClient);
     }
 
     public function createClient(): AllegroClientInterface
     {
-        return new AllegroClient($this->credentials, $this->logger);
+        return new AllegroClient($this->credentials, $this->logger, $this->httpClient);
     }
 }
